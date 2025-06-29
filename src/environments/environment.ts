ba890190cd5aec6dd9d54be32e431e789b9f8862
LukaSirotkovic <lukasirotkovic5@gmail.com>
2025-06-29 23:11:53 +0200

    Install Firebase, connect environment variables, and configure in main.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAO0U-q2kZgVHFqGQC6RWbpJDIK-Hcc63E',
+    authDomain: 'nutritrack-ff40e.firebaseapp.com',
+    projectId: 'nutritrack-ff40e',
+    storageBucket: 'nutritrack-ff40e.firebasestorage.app',
+    messagingSenderId: '757031342575',
+    appId: '1:757031342575:web:afbbb3af6a74f4e700a61a',
+  },
 };
 
 /*
