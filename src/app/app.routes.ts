46acafbb03210df8683a0011279c32c7446bdab1
LukaSirotkovic <lukasirotkovic5@gmail.com>
2025-08-02 14:11:24 +0200

    Removed login guard and onboarding guard from imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
-import { loginGuard } from './guards/login.guard';
-import { onboardingGuard } from './guards/onboarding.guard';
 import { MainWrapperPage } from './pages/main-wrapper/main-wrapper.page';
 import { entryGuard } from './guards/entry.guard';
 
